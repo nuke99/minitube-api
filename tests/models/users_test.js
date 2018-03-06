567c3c5e0d2ec48b2e2491f340be893f27e0d326
Treasure Priyamal <treasure.vuln@gmail.com>
2018-03-06 20:12:14 +0530

    Test - fetch user by id

--- tests/models/users_test.js
+++ tests/models/users_test.js
@@ -44,29 +44,37 @@ describe('Models',() => {
             })
 
             it('should create a user ',(done) => {
-                
-                
-                let user = {
+                let create_user = {
                     name : 'Treasure',
                     email : 'treasure@sampleweb.com',
                     password : 'thisisasamplepassword',
                     token : guest_token,
                     status : 'inactive'
         
-                }
-                
-                Users.create(user).then(_user => {
+                }                
+                Users.create(create_user).then(_user => {
+                    user = _user
                     expect(_user).to.have.property('name')
                     expect(_user).to.have.property('id')
                     expect(_user).to.have.property('email')
                     expect(_user).to.have.property('password')
                     expect(_user).to.have.property('guest_token_id')
                     expect(_user).to.have.property('status')
+                    expect(_user.password).to.equal('d7432b084eda3ad96b53aa55e0c06f42212b14bd')
+                    
                     done()
                 }).catch(error => {
                     throw error
                     done()
                 })
+            })
+
+            it('should fetch the user', (done) => {
+                Users.findUserByID(user.id).then(_user => {
+                    console.log('user',_user);
+                })
+
+
             })
 
         })
